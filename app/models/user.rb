7989e50a7ade2d0963be163331b70b9e59c95c08
Adrian <a.fernandez1@live.com>
2019-04-28 10:31:03 -0700

    added more ideas

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: :true, uniqueness: :true
 
-  has_many :activities
-  has_many :wishlist_itneraries
-  belongs_to :itinerary
-  belongs_to :activity
+  # has_many :activities
+  # has_many :wishlist_itneraries
+  # belongs_to :itinerary
+  # belongs_to :activity
 end
